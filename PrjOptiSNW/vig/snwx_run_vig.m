bbba7ec75090897a0d88c54e2ee9d4e8e6b805ef
Fan Wang <wangfanbsg75@live.com>
2020-07-28 00:07:14 -0500

    splanneralt vig tests

--- PrjOptiSNW/vig/snwx_run_vig.m
+++ PrjOptiSNW/vig/snwx_run_vig.m
@@ -1,44 +1,28 @@
 %% File Paths
-
-git_repo = 'C:\Users\fan\PrjOptiSNW\PrjOptiSNW';
+clc
+% git_repo = 'C:\Users\fan\PrjOptiSNW\PrjOptiSNW';
+git_repo = 'C:\Users\fan\Documents\Dropbox (UH-ECON)\PrjNygaardSorensenWang\PrjOptiSNW\PrjOptiSNW';
 git_repo_vig_main = fullfile(git_repo, 'vig');
 
 dropbox_htmlout = 'C:\Users\fan\Documents\Dropbox (UH-ECON)\PrjNygaardSorensenWang\';
 dorpbox_htmlout_vig_main = 'MatlabVig\07-07-2020';
 
 % st_mlx_search_name = '*_densemore_*.mlx';
-st_mlx_search_name = 'snwx_v_planner_densemore.mlx';
+% st_mlx_search_name = 'snwx_v_planner_densemore.mlx';
+st_mlx_search_name = 'snw_evuvw19_jmky*.mlx';
 st_pub_format = 'html';
 bl_run_mlx = true;
 bl_run_mlx_only = false;
 bl_verbose = true;
 
-%% Run amin Vignette
-% st_proj_folder = git_repo_vig_main;
-% cl_st_subfolder = {'sdist'};
-% st_out_folder = fullfile(dropbox_htmlout, dorpbox_htmlout_vig_main);
-% ff_mlx2htmlpdf_runandexport(...
-%     st_proj_folder, cl_st_subfolder, ...
-%     st_mlx_search_name, st_out_folder, st_pub_format, ...
-%     bl_run_mlx, bl_run_mlx_only, ...
-%     bl_verbose);
+% cl_st_subfolder = {'sdist', 'splanner','svalpol'};
+cl_st_subfolder = {'splanneralt'};
 
-%% Run ds_savingsunkc Vignette
+%% Run amin Vignette
 st_proj_folder = git_repo_vig_main;
-cl_st_subfolder = {'splanner'};
 st_out_folder = fullfile(dropbox_htmlout, dorpbox_htmlout_vig_main);
 ff_mlx2htmlpdf_runandexport(...
     st_proj_folder, cl_st_subfolder, ...
     st_mlx_search_name, st_out_folder, st_pub_format, ...
     bl_run_mlx, bl_run_mlx_only, ...
     bl_verbose);
-
-%% Run savingfc Vignette
-% st_proj_folder = git_repo_vig_main;
-% cl_st_subfolder = {'svalpol'};
-% st_out_folder = fullfile(dropbox_htmlout, dorpbox_htmlout_vig_main);
-% ff_mlx2htmlpdf_runandexport(...
-%     st_proj_folder, cl_st_subfolder, ...
-%     st_mlx_search_name, st_out_folder, st_pub_format, ...
-%     bl_run_mlx, bl_run_mlx_only, ...
-%     bl_verbose);
